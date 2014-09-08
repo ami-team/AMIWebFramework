4e647ac21c5eed042e555b0361f471dc5261025e
Jérôme Odier <odier.jerome@gmail.com>
2014-09-08 10:53:31 +0200

    update

--- js/AMI/AMIWebApp.js
+++ js/AMI/AMIWebApp.js
@@ -406,11 +406,13 @@ function AMIWebApp() {
 			}
 		} else {
 
-			var cookie_value = document.cookie.match('(^|;)[\s]*' + name + '=([^;]*)');
-
-			if(cookie_value) {
-				return decodeURIComponent(cookie_value[2]);
-			}
+name += '=';
+ var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
 		}
 
 		return '';
