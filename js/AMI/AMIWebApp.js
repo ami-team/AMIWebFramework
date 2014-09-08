c8270f2cd14633d695e18e8218765dfdcfb59e03
Jérôme Odier <odier.jerome@gmail.com>
2014-09-08 11:00:52 +0200

    update

--- js/AMI/AMIWebApp.js
+++ js/AMI/AMIWebApp.js
@@ -405,14 +405,21 @@ function AMIWebApp() {
 				}
 			}
 		} else {
+			var cn = name + '=';
 
-name += '=';
- var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
-    }
+			var ca = document.cookie.split(';');
+
+			for(var i = 0; i < ca.length; i++) {
+				var c = ca[i];
+
+				while(c.charAt(0) == ' ') {
+					c = c.substring(1);
+				}
+
+				if(c.indexOf(cn) != -1) {
+					return c.substring(cn.length, c.length);
+				}
+			}
 		}
 
 		return '';
