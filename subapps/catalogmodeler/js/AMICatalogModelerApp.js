60818864cc9ab0470a9de14875ed2dec5751f597
Jérôme Odier <odier.jerome@gmail.com>
2014-09-23 00:46:31 +0200

    update

--- subapps/catalogmodeler/js/AMICatalogModelerApp.js
+++ subapps/catalogmodeler/js/AMICatalogModelerApp.js
@@ -20,7 +20,9 @@ function AMICatalogModelerApp() {
 		amiWebApp.loadSheets([
 			'subapps/catalogmodeler/css/joint.min.css',
 			'subapps/catalogmodeler/css/AMICatalogModelerApp.css',
-		]);
+		]).fail(function(info) {
+			alert(info);
+		});
 
 		amiWebApp.loadScripts([
 			'subapps/catalogmodeler/js/lodash.min.js',
@@ -28,48 +30,47 @@ function AMICatalogModelerApp() {
 			'subapps/catalogmodeler/js/joint.min.js',
 			'subapps/catalogmodeler/js/vectorizer.min.js',
 			'subapps/catalogmodeler/js/geometry.min.js',
+			'subapps/catalogmodeler/js/joint.shapes.sql.min.js',
 		]);
 
 		$('#ami_jumbotron_title').html('Catalog Modeler');
 		$('#ami_jumbotron_content').html('');
 		$('#ami_breadcrumb_content').html('<li><a href="">Admin</a></li><li><a href="">Catalog Modeler</a></li>');
 
-		amiWebApp.loadHTML('subapps/catalogmodeler/html/AMICatalogModelerApp.html', {context: this}).done(function(data) {
+		amiWebApp.loadHTML('subapps/catalogmodeler/html/AMICatalogModelerApp.html', {context: this}).done(function(data1) {
+			amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/table.html', {context: this}).done(function(data2) {
+				amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/field.html', {context: this}).done(function(data3) {
+					amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/fkey.html', {context: this}).done(function(data4) {
 
-			amiWebApp.replaceHTML('ami_main_content', data, {context: this}).done(function() {
+						amiWebApp.replaceHTML('ami_main_content', data1, {context: this}).done(function() {
 
-				this.graph = new joint.dia.Graph;
+							this.fragmentTable = data2;
+							this.fragmentField = data3;
+							this.fragmentFKey = data4;
 
-				this.paper = new joint.dia.Paper({
-					el: $('#sqldiagram'),
-					width: '100%',
-					height: '450',
-					model: this.graph,
-					gridSize: 10
+							this.graph = new joint.dia.Graph;
+
+							this.paper = new joint.dia.Paper({
+								model: this.graph,
+								el: $('#sqldiagram'),
+								width: '100%',
+								height: '550',
+								gridSize: 10,
+							});
+
+							this.paper.on('cell:pointerdown', function(cellView, evt, x, y) {
+								amiCatalogModelerApp.updateMenu(cellView.model);
+							});
+
+							this.svg = V(this.paper.svg);
+
+							this.svgVertical = V('path').attr('d', 'M -10000 -1 L 10000 -1');
+							this.svgHorizontal = V('path').attr('d', 'M -1 -10000 L -1 10000');
+
+							this.drawGrid(10, 10);
+						});
+					});
 				});
-
-				this.svg = V(this.paper.svg);
-
-				var rect1 = new joint.shapes.basic.Rect({
-					position: { x: 100, y: 20 },
-					size: { width: 100, height: 40 },
-					attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-				});
-
-				var rect2 = rect1.clone();
-				rect2.translate(300);
-
-				var link = new joint.dia.Link({
-					source: { id: rect1.id },
-					target: { id: rect2.id },
-				});
-
-				this.svgVertical = V('path').attr('d', 'M -10000 -1 L 10000 -1');
-				this.svgHorizontal = V('path').attr('d', 'M -1 -10000 L -1 10000');
-
-				this.drawGrid(10, 10);
-
-				this.graph.addCells([rect1, rect2, link]);
       			});
 		});
 	};
@@ -105,8 +106,7 @@ function AMICatalogModelerApp() {
 			var x = gridW;
 
 			do {
-
-				var svgGridX = this.svgHorizontal.clone().translate(x, 0, {absolute: true}).addClass('my_grid');
+				var svgGridX = this.svgHorizontal.clone().translate(x, 0, {absolute: true}).addClass('sql_editor_grid');
 				this.svg.append(svgGridX);
 			
 				x += gridW;
@@ -121,8 +121,7 @@ function AMICatalogModelerApp() {
 			var y = gridH;
 
 			do {
-
-				var svgGridY = this.svgVertical.clone().translate(0, y, {absolute: true}).addClass('my_grid');
+				var svgGridY = this.svgVertical.clone().translate(0, y, {absolute: true}).addClass('sql_editor_grid');
 				this.svg.append(svgGridY);
 
 				y += gridH;
@@ -131,6 +130,66 @@ function AMICatalogModelerApp() {
 		}
 
 		/*---------------------------------------------------------*/
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this.updateMenu = function(table) {
+
+		var dict1 = {
+			TABLE: table.getName(),
+		};
+
+		var dict2 = [];
+		var dict3 = [];
+
+		$.each(table.getFields(), function(nr, field) {
+
+			dict2.push({
+				FIELD: field['name'],
+				TYPE: field['type'],
+			});
+
+			dict3.push({
+				/* TODO */
+			});
+		});
+
+		var divs = $('#ami-catalog-modeler-accordion table');
+
+		divs[0].innerHTML = amiWebApp.formatHTML(this.fragmentTable, {dict: dict1});
+		divs[1].innerHTML = amiWebApp.formatHTML(this.fragmentField, {dict: dict2});
+		divs[2].innerHTML = amiWebApp.formatHTML(this.fragmentFKey, {dict: dict3});
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this._cnt = 0;
+
+	/*-----------------------------------------------------------------*/
+
+	this.addTable = function() {
+
+		var table = new joint.shapes.sql.Table({
+			position: {x: 20, y: 20},
+			name: 'Table' + this._cnt++,
+			fields: [
+				{name: 'id', type: 'int'},
+				{name: 'name', type: 'str'},
+			],
+		});
+
+		this.graph.addCells([table]);
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this.viewSQL = function() {
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this.synchronize = function() {
 	};
 
 	/*-----------------------------------------------------------------*/
