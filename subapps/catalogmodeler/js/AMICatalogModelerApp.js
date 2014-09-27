77c661d0a13a4b154abeaf74f31100e37ec8096e
Jérôme Odier <odier.jerome@gmail.com>
2014-09-27 20:30:14 +0200

    update

--- subapps/catalogmodeler/js/AMICatalogModelerApp.js
+++ subapps/catalogmodeler/js/AMICatalogModelerApp.js
@@ -40,55 +40,62 @@ function AMICatalogModelerApp() {
 			amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/table.html', {context: this}).done(function(data2) {
 				amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/field.html', {context: this}).done(function(data3) {
 					amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/fkey.html', {context: this}).done(function(data4) {
+						amiWebApp.loadHTML('subapps/catalogmodeler/html/Fragment/index.html', {context: this}).done(function(data5) {
 
-						amiWebApp.replaceHTML('ami_main_content', data1, {context: this}).done(function() {
-							/*-----------------*/
+							amiWebApp.replaceHTML('ami_main_content', data1, {context: this}).done(function() {
+								/*---------*/
 
-							this.fragmentTable = data2;
-							this.fragmentField = data3;
-							this.fragmentFKey = data4;
+								this.fragmentTable = data2;
+								this.fragmentField = data3;
+								this.fragmentFKey = data4;
+								this.fragmentIndex = data5;
 
-							/*-----------------*/
+								/*---------*/
 
-							var dropZone = document.getElementById('drop_zone');
+								var dropZone = document.getElementById('drop_zone');
 
-							dropZone.addEventListener('dragover', this.handleDragOver, false);
-							dropZone.addEventListener('drop', this.handleFileSelect, false);
+								dropZone.addEventListener('dragover', this.handleDragOver, false);
+								dropZone.addEventListener('drop', this.handleFileSelect, false);
   
-							/*-----------------*/
+								/*---------*/
 
-							this.graph = new joint.dia.Graph;
+								this.graph = new joint.dia.Graph;
 
-							this.paper = new joint.dia.Paper({
-								model: this.graph,
-								el: $('#editor_zone'),
-								width: 900,
-								height: 418,
-								gridSize: 5.0,
+								this.paper = new joint.dia.Paper({
+									model: this.graph,
+									el: $('#editor_zone'),
+									width: 900,
+									height: 418,
+									gridSize: 5.0,
+								});
+
+								this.svg = V(this.paper.svg);
+
+								/*---------*/
+
+								this.svgVertical = V('path').attr('d', 'M -10000 -1 L 10000 -1');
+								this.svgHorizontal = V('path').attr('d', 'M -1 -10000 L -1 10000');
+
+								this.drawGrid(10, 10);
+
+								/*---------*/
+
+								this.table = undefined;
+
+								this.paper.on('cell:pointerclick', function(cellView) {
+
+									if(cellView.model.get('type') === 'sql.Table') {
+										amiCatalogModelerApp.table = cellView.model;
+										amiCatalogModelerApp.updateMenu();
+									}
+								});
+
+								/*---------*/
+
+								this.listSchemes();
+
+								/*---------*/
 							});
-
-							this.svg = V(this.paper.svg);
-
-							/*-----------------*/
-
-							this.svgVertical = V('path').attr('d', 'M -10000 -1 L 10000 -1');
-							this.svgHorizontal = V('path').attr('d', 'M -1 -10000 L -1 10000');
-
-							this.drawGrid(10, 10);
-
-							/*-----------------*/
-
-							this.table = undefined;
-
-							this.paper.on('cell:pointerclick', function(cellView) {
-
-								if(cellView.model.get('type') === 'sql.Table') {
-									amiCatalogModelerApp.table = cellView.model;
-									amiCatalogModelerApp.updateMenu();
-								}
-							});
-
-							/*-----------------*/
 						});
 					});
 				});
@@ -152,7 +159,30 @@ function AMICatalogModelerApp() {
 		/*---------------------------------------------------------*/
 	};
 
-	/*-------------------------------------------------------------------------*/
+	/*-----------------------------------------------------------------*/
+
+	this.listSchemes = function() {
+
+		$('#ami-catalog-modeler-catalog-list').empty();
+
+		amiCommand.execute('SearchQuery -project="self" -processingStep="self" -glite="SELECT router_db.db, router_db.jsonSchema WHERE (1=1)"').done(function(data) {
+
+			var rows = amiWebApp.jspath('..row', data);
+
+			$.each(rows, function(index, row) {
+
+				var name = amiWebApp.jspath('..field{.@name==="db"}.$', row)[0];
+				var json = amiWebApp.jspath('..field{.@name==="jsonSchema"}.$', row)[0];
+
+				$('#ami-catalog-modeler-catalog-list').append('<option value="' + index + '">' + name + '</option>');
+			});
+
+		}).fail(function(data) {
+			alert(JSPath.apply('..error.$', data)[0]);
+		});
+	};
+
+	/*-----------------------------------------------------------------*/
 
 	this.handleFileSelect = function(e) {
 		e.stopPropagation();
@@ -218,7 +248,6 @@ function AMICatalogModelerApp() {
 	this.addField = function() {
 
 		if(this.table) {
-
 			var fields = this.table.getFields();
 
 			fields.push({name: 'field' + this._cnt2++, type: 'INT'});
@@ -236,7 +265,6 @@ function AMICatalogModelerApp() {
 	this.addForeignKey = function() {
 
 		if(this.table) {
-
 			var fields = this.table.getFKeys();
 
 			fields.push({field: '', table: ''});
@@ -284,12 +312,7 @@ function AMICatalogModelerApp() {
 
 	this.synchronize = function() {
 
-		var json = this.graph.toJSON();
-
-		this.graph.clear();
-		alert(JSON.stringify(json));
-
-		this.graph.fromJSON(json);
+		alert('* TODO *');
 	};
 
 	/*-----------------------------------------------------------------*/
@@ -313,6 +336,7 @@ function AMICatalogModelerApp() {
 			var dict1 = {};
 			var dict2 = [];
 			var dict3 = [];
+			var dict4 = [];
 
 			var tables = '<option value="">--</option>';
 			var fields = '<option value="">--</option>';
@@ -337,9 +361,9 @@ function AMICatalogModelerApp() {
 
 			/*-------------------------------------------------*/
 
-			$.each(this.graph.getElements(), function(index, table) {
+			$.each(this.graph.getElements(), function(index, item) {
 
-				var name = table.getName();
+				var name = item.getName();
 
 				if(tableName != name) {
 					tables += '<option value="' + name + '">' + name + '</option>';
@@ -348,10 +372,10 @@ function AMICatalogModelerApp() {
 
 			/*-------------------------------------------------*/
 
-			$.each(this.table.getFields(), function(index, field) {
+			$.each(this.table.getFields(), function(index, item) {
 
-				var name = field['name'];
-				var type = field['type'];
+				var name = item['name'];
+				var type = item['type'];
 
 				dict2.push({
 					INDEX: index,
@@ -364,10 +388,10 @@ function AMICatalogModelerApp() {
 
 			/*-------------------------------------------------*/
 
-			$.each(this.table.getFKeys(), function(index, fkey) {
+			$.each(this.table.getFKeys(), function(index, item) {
 
-				var field = fkey['field'];
-				var table = fkey['table'];
+				var field = item['field'];
+				var table = item['table'];
 
 				dict3.push({
 					INDEX: index,
@@ -378,11 +402,24 @@ function AMICatalogModelerApp() {
 
 			/*-------------------------------------------------*/
 
+			$.each(this.table.getIndices(), function(index, item) {
+
+				var field = item['field'];
+
+				dict4.push({
+					INDEX: index,
+					FIELDS: fields.replace('value="' + field + '"', 'value="' + field + '" selected="selected"'),
+				});
+			});
+
+			/*-------------------------------------------------*/
+
 			var divs = $('#ami-catalog-modeler-tab-properties table tbody');
 
 			if(!soft) {
 				divs[0].innerHTML = amiWebApp.formatHTML(this.fragmentTable, {dict: dict1});
 				divs[1].innerHTML = amiWebApp.formatHTML(this.fragmentField, {dict: dict2});
+				divs[3].innerHTML = amiWebApp.formatHTML(this.fragmentIndex, {dict: dict4});
 			}
 			if(!0x00) {
 				divs[2].innerHTML = amiWebApp.formatHTML(this.fragmentFKey, {dict: dict3});
@@ -479,12 +516,11 @@ function AMICatalogModelerApp() {
 	this.setFieldName = function(value, index) {
 
 		if(this.table) {
+			var items = this.table.getFields();
 
-			var fields = this.table.getFields();
-
-			fields[index]['name'] = value;
+			items[index]['name'] = value;
 			
-			this.table.setFields(fields);
+			this.table.setFields(items);
 			this.updateMenu({soft: true});
 		}
 	};
@@ -494,12 +530,11 @@ function AMICatalogModelerApp() {
 	this.setFieldType = function(value, index) {
 
 		if(this.table) {
+			var items = this.table.getFields();
 
-			var fields = this.table.getFields();
-
-			fields[index]['type'] = value;
+			items[index]['type'] = value;
 			
-			this.table.setFields(fields);
+			this.table.setFields(items);
 			this.updateMenu({soft: true});
 		}
 	};
@@ -509,11 +544,11 @@ function AMICatalogModelerApp() {
 	this.removeField = function(index) {
 
 		if(this.table) {
-			var fields = this.table.getFields();
+			var items = this.table.getFields();
 
-			fields.splice(index, 1);
+			items.splice(index, 1);
 			
-			this.table.setFields(fields);
+			this.table.setFields(items);
 			this.updateMenu({soft: false});
 			this.updateArrows();
 		}
@@ -524,11 +559,11 @@ function AMICatalogModelerApp() {
 	this.setFKeyField = function(value, index) {
 
 		if(this.table) {
-			var fKeys = this.table.getFKeys();
+			var items = this.table.getFKeys();
 
-			fKeys[index]['field'] = value;
+			items[index]['field'] = value;
 
-			this.table.setFKeys(fKeys);
+			this.table.setFKeys(items);
 			this.updateArrows();
 		}
 	};
@@ -538,11 +573,11 @@ function AMICatalogModelerApp() {
 	this.setFKeyTable = function(value, index) {
 
 		if(this.table) {
-			var fKeys = this.table.getFKeys();
+			var items = this.table.getFKeys();
 
-			fKeys[index]['table'] = value;
+			items[index]['table'] = value;
 
-			this.table.setFKeys(fKeys);
+			this.table.setFKeys(items);
 			this.updateArrows();
 		}
 	};
@@ -552,12 +587,41 @@ function AMICatalogModelerApp() {
 	this.removeFKey = function(index) {
 
 		if(this.table) {
-			var fKeys = this.table.getFKeys();
+			var items = this.table.getFKeys();
 
-			fKeys.splice(index, 1);
+			items.splice(index, 1);
 			
-			this.table.setFKeys(fKeys);
+			this.table.setFKeys(items);
 			this.updateMenu({soft: false});
+			this.updateArrows();
+		}
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this.setIndexField = function(value, index) {
+
+		if(this.table) {
+			var items = this.table.getIndices();
+
+			items[index]['field'] = value;
+
+			this.table.setIndices(items);
+			this.updateArrows();
+		}
+	};
+
+	/*-----------------------------------------------------------------*/
+
+	this.removeFKey = function(index) {
+
+		if(this.table) {
+			var items = this.table.getIndices();
+
+			items.splice(index, 1);
+			
+			this.table.setIndices(items);
+			this.updateMenu({soft: true});
 			this.updateArrows();
 		}
 	};
