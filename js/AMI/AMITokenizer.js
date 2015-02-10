d88967138de26e216b2981718ec4801e1532ff9b
Jérôme Odier <odier.jerome@gmail.com>
2015-02-10 16:22:51 +0100

    update

--- js/AMI/AMITokenizer.js
+++ js/AMI/AMITokenizer.js
@@ -13,9 +13,10 @@
 function AMITokenizer() {
 	/*-----------------------------------------------------------------*/
 
-	this.tokenize = function(s, spaces, kwords, quotes, escape) {
+	this.tokenize = function(s, spaces, kwords, quotes, escape, line) {
 
-		var result = [];
+		var result_tokens = [];
+		var result_lines = [];
 
 		var word = "";
 		var found = false;
@@ -23,7 +24,19 @@ function AMITokenizer() {
 		var i = 0x000000;
 		var l = s.length;
 
+		if(!line) {
+			line = 1;
+		}
+
 		while(i < l) {
+			/*-------------------------------------------------*/
+			/* COUNT LINES                                     */
+			/*-------------------------------------------------*/
+
+			if(s.charAt(i) === '\n') {
+				line += 1;
+			}
+
 			/*-------------------------------------------------*/
 			/* EAT SPACES                                      */
 			/*-------------------------------------------------*/
@@ -31,7 +44,8 @@ function AMITokenizer() {
 			if(s.charAt(i) in spaces) {
 
 				if(word) {
-					result.push(word);
+					result_tokens.push(word);
+					result_lines.push(line);
 					word = '';
 				}
 
@@ -46,23 +60,28 @@ function AMITokenizer() {
 
 			found = false;
 
-			$.arrayFor(kwords, function(index, kword) {
+			for(var idx = 0; idx < kwords.length; idx++) {
+				kword = kwords[i];
 
 				if(s.substring(i).indexOf(kword) === 0) {
 
 					if(word) {
-						result.push(word);
+						result_tokens.push(word);
+						result_lines.push(line);
 						word = '';
 					}
 
 					var j = i + kword.length;
-					result.push(s.substring(i, j));
+
+					result_tokens.push(s.substring(i, j));
+					result_lines.push(line);
+
 					i = j;
 
 					found = true;
-					return;
+					break;
 				}
-			});
+			}
 
 			if(found) {
 				continue;
@@ -74,23 +93,28 @@ function AMITokenizer() {
 
 			found = false;
 
-			$.arrayFor(quotes, function(index, quote) {
+			for(var idx = 0; idx < quotes.length; idx++) {
+				quote = quotes[i];
 
 				if(s.substring(i).indexOf(quote) === 0) {
 
 					if(word) {
-						result.push(word);
+						result_tokens.push(word);
+						result_lines.push(line);
 						word = '';
 					}
 
-					var j = i + amiTokenizer.shift(s.substring(i), quote, escape);
-					result.push(s.substring(i, j));
+					var j = i + amiTokenizer.shift(s.substring(i), quote, escape, line);
+
+					result_tokens.push(s.substring(i, j));
+					result_lines.push(line);
+
 					i = j;
 
 					found = true;
-					return;
+					break;
 				}
-			});
+			}
 
 			if(found) {
 				continue;
@@ -106,15 +130,16 @@ function AMITokenizer() {
 		}
 
 		if(word) {
-			result.push(word);
+			result_tokens.push(word);
+			result_lines.push(line);
 		}
 
-		return result;
+		return result_tokens;
 	};
 
 	/*-----------------------------------------------------------------*/
 
-	this.shift = function(s, quote, escape) {
+	this.shift = function(s, quote, escape, line) {
 
 		var l = s.length;
 		var m = quote.length;
@@ -140,7 +165,7 @@ function AMITokenizer() {
 			}
 		}
 
-		throw 'syntax error, missing token `' + quote + '`';
+		throw 'syntax error, line `' + line + '`, missing token `' + quote + '`';
 	};
 
 	/*-----------------------------------------------------------------*/
