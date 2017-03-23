714ee42c2b86cbd847887bb2d9b6a699f2dffe5a
Jérôme Odier <odier.jerome@gmail.com>
2017-03-23 15:41:37 +0100

    update

---
+++ tools/newsubapp/src/main/java/net/hep/ami/awf/NewSubApp.java
@@ -0,0 +1,155 @@
+package net.hep.ami.awf;
+
+import java.io.*;
+
+import java.util.*;
+
+public class NewSubApp
+{
+	/*---------------------------------------------------------------------*/
+
+	public static void createNewSubApp(String x) throws Exception
+	{
+		/*-----------------------------------------------------------------*/
+
+		if(x.length() == 0)
+		{
+			throw new Exception("");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		String name = x.substring(0, 1).toLowerCase() + x.substring(1);
+		String NAME = x.substring(0, 1).toUpperCase() + x.substring(1);
+
+		/*-----------------------------------------------------------------*/
+
+		String YEAR = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+
+		/*-----------------------------------------------------------------*/
+
+		File baseDir = new File(NewSubApp.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+
+		while((baseDir = baseDir.getParentFile()) != null)
+		{
+			File temp = new File(baseDir, "subapps");
+
+			if(temp.exists())
+			{
+				baseDir = temp;
+
+				break;
+			}
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		File appDir = new File(baseDir, NAME);
+
+		if(appDir.mkdir() == false)
+		{
+			throw new Exception("");
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		File jsDir = new File(appDir, "js");
+		File cssDir = new File(appDir, "css");
+		File twigDir = new File(appDir, "twig");
+
+		jsDir.mkdir();
+		cssDir.mkdir();
+		twigDir.mkdir();
+
+		/*-----------------------------------------------------------------*/
+
+		StringBuilder stringBuilder1 = new StringBuilder();
+		NewSubApp.loadResource(stringBuilder1, "/net/hep/ami/awf/app.js");
+
+		StringBuilder stringBuilder2 = new StringBuilder();
+		NewSubApp.loadResource(stringBuilder2, "/net/hep/ami/awf/app.css");
+
+		StringBuilder stringBuilder3 = new StringBuilder();
+		NewSubApp.loadResource(stringBuilder3, "/net/hep/ami/awf/app.twig");
+
+		/*-----------------------------------------------------------------*/
+
+		FileWriter fileWriter1 = new FileWriter(new File(jsDir, NAME + "App.js"), false);
+
+		try {
+			fileWriter1.write(stringBuilder1.toString().replace("{{name}}", name).replace("{{NAME}}", NAME).replace("{{YEAR}}", YEAR));
+		}
+		finally {
+			fileWriter1.close();
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		FileWriter fileWriter2 = new FileWriter(new File(cssDir, NAME + "App.css"), false);
+
+		try {
+			fileWriter2.write(stringBuilder2.toString().replace("{{name}}", name).replace("{{NAME}}", NAME).replace("{{YEAR}}", YEAR));
+		}
+		finally {
+			fileWriter2.close();
+		}
+
+		/*-----------------------------------------------------------------*/
+
+		FileWriter fileWriter3 = new FileWriter(new File(twigDir, NAME + "App.twig"), false);
+
+		try {
+			fileWriter3.write(stringBuilder3.toString().replace("{{name}}", name).replace("{{NAME}}", NAME).replace("{{YEAR}}", YEAR));
+		}
+		finally {
+			fileWriter3.close();
+		}
+
+		/*-----------------------------------------------------------------*/
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	private static void loadResource(StringBuilder stringBuilder, String path) throws Exception
+	{
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(NewSubApp.class.getResourceAsStream(path)));
+
+		try
+		{
+			String line;
+
+			while((line = bufferedReader.readLine()) != null)
+			{
+				stringBuilder.append(line)
+				             .append('\n')
+				;
+			}
+		}
+		finally
+		{
+			bufferedReader.close();
+		}
+	}
+
+	/*---------------------------------------------------------------------*/
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			System.out.print("Enter the sub app name ([a-zA-Z][a-zA-Z0-9]*): ");
+
+			NewSubApp.createNewSubApp(new Scanner(System.in).nextLine());
+		}
+		catch(Exception e)
+		{
+			System.err.println(e.getMessage());
+
+			System.exit(1);
+		}
+
+		System.exit(0);
+	}
+
+	/*---------------------------------------------------------------------*/
+}
