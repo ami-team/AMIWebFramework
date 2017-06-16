b7ba8dc73dcff480a363c1508f8354ad8b16aa5c
Jérôme Odier <odier.jerome@gmail.com>
2017-06-16 22:32:56 +0200

    update

--- js/AMI/AMILogin.js
+++ js/AMI/AMILogin.js
@@ -57,12 +57,12 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 			amiCommand.certLogin().always(function(data, userInfo, roleInfo, ssoInfo) {
 				/*-----------------------------------------*/
 
-				$('#8008BBBC_FF0D_481C_8AF0_3DCF7D6A1676').submit(function(e) {
+				$('#B7894CC1_1DAA_4A7E_B7D1_DBDF6F06AC73').submit(function(e) {
 
 					amiLogin.form_login(e);
 				});
 
-				$('#2D609FA7_9BA6_4B0B_A0F2_094403F81E7D').submit(function(e) {
+				$('#EE055CD4_E58F_4834_8020_986AE3F8D67D').submit(function(e) {
 
 					amiLogin.form_addUser(e);
 				});
@@ -72,21 +72,21 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 					amiLogin.form_remindPass(e);
 				});
 
-				$('#46A4D44F_BCE4_4407_AB70_194347BFC12C').submit(function(e) {
+				$('#D9EAF998_ED8E_44D2_A0BE_8C5CF5E438BD').submit(function(e) {
 
 					amiLogin.form_changeInfo(e);
 				});
 
-				$('#0CD4F500_5EEA_49AF_8853_225572FDC2E7').submit(function(e) {
+				$('#E92A1097_983B_4857_875F_07E4659B41B0').submit(function(e) {
 
 					amiLogin.form_changePass(e);
 				});
 
 				/*-----------------------------------------*/
 
-				$('#56CE3A89_05AC_46F2_811B_D0D8DBAECEB3,#CCD8E6F1_6DF8_4BDD_A0EC_C3C380830187').change(function() {
+				$('#E6E30EEC_15EE_4FCF_9809_2B8EC2FEF388,#CCD8E6F1_6DF8_4BDD_A0EC_C3C380830187').change(function() {
 
-					var pass1 = $('#56CE3A89_05AC_46F2_811B_D0D8DBAECEB3').val();
+					var pass1 = $('#E6E30EEC_15EE_4FCF_9809_2B8EC2FEF388').val();
 					var pass2 = $('#CCD8E6F1_6DF8_4BDD_A0EC_C3C380830187').val();
 
 					$('#CCD8E6F1_6DF8_4BDD_A0EC_C3C380830187').get(0).setCustomValidity(
@@ -94,12 +94,12 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 					);
 				});
 
-				$('#D487FE72_8D95_4048_BEA3_252274862AF4,#9A355DC8_88EF_4438_BAB3_BB7BAD798A77').change(function() {
+				$('#D487FE72_8D95_4048_BEA3_252274862AF4,#EE1DA58C_3761_4734_A9C2_E808CDD7EE77').change(function() {
 
 					var pass1 = $('#D487FE72_8D95_4048_BEA3_252274862AF4').val();
-					var pass2 = $('#9A355DC8_88EF_4438_BAB3_BB7BAD798A77').val();
+					var pass2 = $('#EE1DA58C_3761_4734_A9C2_E808CDD7EE77').val();
 
-					$('#9A355DC8_88EF_4438_BAB3_BB7BAD798A77').get(0).setCustomValidity(
+					$('#EE1DA58C_3761_4734_A9C2_E808CDD7EE77').get(0).setCustomValidity(
 						pass1.length > 0 && pass2.length > 0 && pass1 !== pass2 ? 'Passwords don\'t match.' : ''
 					);
 				});
@@ -145,9 +145,9 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 			message = message.join('. ');
 		}
 
-		amiWebApp.replaceHTML('#4A634334_ED84_4565_A2B3_618ADFDF1E9B', amiWebApp.fragmentSuccess, {dict: {MESSAGE: message}});
+		amiWebApp.replaceHTML('#C0D13C0C_BA64_4A79_BF48_1A35F26D19AC', amiWebApp.fragmentSuccess, {dict: {MESSAGE: message}});
 
-		$('#4A634334_ED84_4565_A2B3_618ADFDF1E9B .alert').fadeOut(45000);
+		$('#C0D13C0C_BA64_4A79_BF48_1A35F26D19AC .alert').fadeOut(45000);
 
 		amiLogin._clean();
 		amiWebApp.unlock();
@@ -162,9 +162,9 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 			message = message.join('. ');
 		}
 
-		amiWebApp.replaceHTML('#4A634334_ED84_4565_A2B3_618ADFDF1E9B', amiWebApp.fragmentError, {dict: {MESSAGE: message}});
+		amiWebApp.replaceHTML('#C0D13C0C_BA64_4A79_BF48_1A35F26D19AC', amiWebApp.fragmentError, {dict: {MESSAGE: message}});
 
-		$('#4A634334_ED84_4565_A2B3_618ADFDF1E9B .alert').fadeOut(45000);
+		$('#C0D13C0C_BA64_4A79_BF48_1A35F26D19AC .alert').fadeOut(45000);
 
 		amiLogin._clean();
 		amiWebApp.unlock();
@@ -242,7 +242,7 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 
 	_flush: function()
 	{
-		$('#4A634334_ED84_4565_A2B3_618ADFDF1E9B').empty();
+		$('#C0D13C0C_BA64_4A79_BF48_1A35F26D19AC').empty();
 		$('#C76F40DA_0480_4D3F_A74B_65735465EA25').empty();
 		$('#F14D98EC_5751_4C15_B4A1_927BA76AFCA6').empty();
 	},
@@ -251,12 +251,12 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 
 	_clean: function()
 	{
-		$('#6E06F758_BF71_4DBA_AF8A_2F137F1ABD1D').val('');
-		$('#56CE3A89_05AC_46F2_811B_D0D8DBAECEB3').val('');
+		$('#A4DFD039_034F_4D10_9668_385AEF4FBBB9').val('');
+		$('#E6E30EEC_15EE_4FCF_9809_2B8EC2FEF388').val('');
 		$('#CCD8E6F1_6DF8_4BDD_A0EC_C3C380830187').val('');
 		$('#F238E6EE_44BD_486A_B85D_C927A4D045D3').val('');
 		$('#D487FE72_8D95_4048_BEA3_252274862AF4').val('');
-		$('#9A355DC8_88EF_4438_BAB3_BB7BAD798A77').val('');
+		$('#EE1DA58C_3761_4734_A9C2_E808CDD7EE77').val('');
 	},
 
 	/*-----------------------------------------------------------------*/
@@ -314,21 +314,21 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 			/*-------------------------------------------------*/
 
 			$('#E513F27D_5521_4B08_BF61_52AFB81356F7').val(firstName);
-			$('#113C64A6_3B10_4671_B941_17C2281A43B6').val(lastName);
-			$('#83B6FEE5_2BAF_4F47_BCB3_1F475A1A1117').val(email);
+			$('#AFF0B5C0_BEEC_4842_916D_DCBA7F589195').val(lastName);
+			$('#C587486B_62C0_4B6E_9288_D8F9F89D157B').val(email);
 
 			/*-------------------------------------------------*/
 
-			$('#215C261E_5DFB_4AC5_85C5_3D3D8C210B82').val(firstName);
-			$('#2DC4C04B_8701_4C50_9D0F_F135D91DA9CE').val(lastName);
-			$('#431ABB43_415D_4564_9F7E_BC8048602120').val(email);
+			$('#ABEB0291_40B0_414A_A42B_E7EABB9B487E').val(firstName);
+			$('#A5AFDB62_1034_4F66_A3E6_9341B31FA290').val(lastName);
+			$('#D730A774_05EA_47AB_A0C8_D92753802E3E').val(email);
 
 			/*-------------------------------------------------*/
 
-			$('#0902CF2E_7A3B_476E_A03B_875FD8928996').val(clientDNInAMI);
-			$('#50CDFAB2_A81D_4772_871A_70F40AEC1C77').val(clientDNInSession);
+			$('#D1BEE3BF_9161_41DC_BC53_C44FFE4D2522').val(clientDNInAMI);
+			$('#C76805D7_1E86_4231_9071_1D04783423BB').val(clientDNInSession);
 			$('#F42FAF6B_2C8D_4142_8BD9_E5BCDCAA05AA').val(issuerDNInAMI);
-			$('#15FA5B80_8528_4FA1_894F_516FD00D1E8D').val(issuerDNInSession);
+			$('#FE2F6232_C256_4B80_939C_EBEC90320308').val(issuerDNInSession);
 
 			/*-------------------------------------------------*/
 
@@ -366,11 +366,11 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 					}
 				}
 
-				$('#02C7F515_F604_4CDF_868A_D8FFE57F9C01').html(
+				$('#D944B01D_2E8D_4EE9_9DCC_2691438BBA16').html(
 					message ? '<span class="fa fa-exclamation-triangle" style="color: orange;"></span> ' + message : ''
 				);
 
-				$('#07D7B8B9_2A2E_4C1A_9DE8_A7970956798D').html(
+				$('#F3FF9F43_DE72_40BB_B1BA_B7B3C9002671').html(
 					'<span style="color: #006400;">valid</span>'
 				);
 
@@ -402,11 +402,11 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 					message = 'Contact the AMI team.';
 				}
 
-				$('#02C7F515_F604_4CDF_868A_D8FFE57F9C01').html(
+				$('#D944B01D_2E8D_4EE9_9DCC_2691438BBA16').html(
 					message ? '<span class="fa fa-exclamation-triangle" style="color: red;"></span> ' + message : ''
 				);
 
-				$('#07D7B8B9_2A2E_4C1A_9DE8_A7970956798D').html(
+				$('#F3FF9F43_DE72_40BB_B1BA_B7B3C9002671').html(
 					'<span style="color: #8B0000;">invalid</span>'
 				);
 
@@ -567,7 +567,7 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 		this._flush();
 		this._clean();
 
-		$('#46A4D44F_BCE4_4407_AB70_194347BFC12C').modal('show');
+		$('#D9EAF998_ED8E_44D2_A0BE_8C5CF5E438BD').modal('show');
 	},
 
 	/*-----------------------------------------------------------------*/
@@ -581,7 +581,7 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 		this._flush();
 		this._clean();
 
-		$('#0CD4F500_5EEA_49AF_8853_225572FDC2E7').modal('show');
+		$('#E92A1097_983B_4857_875F_07E4659B41B0').modal('show');
 	},
 
 	/*-----------------------------------------------------------------*/
@@ -668,7 +668,7 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 
 		var values = this._serializeForm($(e.target));
 
-		return amiCommand.form_login(values['user'], values['pass']);
+		return this.form_login2(values['user'], values['pass']);
 	},
 
 	/*-----------------------------------------------------------------*/
@@ -733,8 +733,8 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 	{
 		/*---------------------------------------------------------*/
 
-		var user = $('#87D39A00_39D9_453B_8D8C_12B403F9268F').val();
-		var pass = $('#6E06F758_BF71_4DBA_AF8A_2F137F1ABD1D').val();
+		var user = $('#E64F24B2_33E6_4DED_9B24_28BE04219613').val();
+		var pass = $('#A4DFD039_034F_4D10_9668_385AEF4FBBB9').val();
 
 		if(!user || !pass)
 		{
@@ -765,8 +765,8 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 	{
 		/*---------------------------------------------------------*/
 
-		var user = $('#87D39A00_39D9_453B_8D8C_12B403F9268F').val();
-		var pass = $('#6E06F758_BF71_4DBA_AF8A_2F137F1ABD1D').val();
+		var user = $('#E64F24B2_33E6_4DED_9B24_28BE04219613').val();
+		var pass = $('#A4DFD039_034F_4D10_9668_385AEF4FBBB9').val();
 
 		if(!user || !pass)
 		{
