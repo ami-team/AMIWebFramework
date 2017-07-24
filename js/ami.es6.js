1f69f38c65fcfa89ba07d0e36fed9266783b8c98
Jérôme Odier <odier.jerome@gmail.com>
2017-07-24 10:51:37 +0200

    update

--- js/ami.es6.js
+++ js/ami.es6.js
@@ -1606,117 +1606,6 @@ amiTwig.expr.Node = function(nodeType, nodeValue) {
  */
 
 /*-------------------------------------------------------------------------*/
-/* amiTwig.ajax                                                            */
-/*-------------------------------------------------------------------------*/
-
-/**
- * The AMI TWIG Ajax
- * @namespace ami/twig/ajax
- */
-
-amiTwig.ajax = {
-	/*-----------------------------------------------------------------*/
-
-	dict: {},
-
-	/*-----------------------------------------------------------------*/
-
-	get: function(url, done, fail)
-	{
-		var txt;
-
-		/*---------------------------------------------------------*/
-
-		if(url in this.dict)
-		{
-			if(done)
-			{
-				done(this.dict[url]);
-			}
-		}
-
-		/*---------------------------------------------------------*/
-
-		if(amiTwig.fs)
-		{
-			/*-------------------------------------------------*/
-			/* NODEJS                                          */
-			/*-------------------------------------------------*/
-
-			try
-			{
-				txt = this.dict[url] = amiTwig.fs.readFileSync(url, 'utf8');
-
-				if(done)
-				{
-					done(txt);
-				}
-			}
-			catch(err)
-			{
-				if(fail)
-				{
-					fail(err);
-				}
-			}
-
-			/*-------------------------------------------------*/
-		}
-		else
-		{
-			/*-------------------------------------------------*/
-			/* BROWSER                                         */
-			/*-------------------------------------------------*/
-
-			var xmlHttpRequest = new XMLHttpRequest();
-
-			xmlHttpRequest.open('GET', url, false);
-			xmlHttpRequest.send();
-
-			/*-------------------------------------------------*/
-
-			if(xmlHttpRequest.status === 200)
-			{
-				txt = this.dict[url] = xmlHttpRequest.responseText;
-
-				if(done)
-				{
-					done(txt);
-				}
-			}
-			else
-			{
-				txt = /**************/ xmlHttpRequest.responseText;
-
-				if(fail)
-				{
-					fail(txt);
-				}
-			}
-
-			/*-------------------------------------------------*/
-		}
-
-		/*---------------------------------------------------------*/
-	},
-
-	/*-----------------------------------------------------------------*/
-};
-
-/*-------------------------------------------------------------------------*/
-
-/*
- * AMI TWIG Engine
- *
- * Copyright (c) 2014-2017 The AMI Team / LPSC / IN2P3
- *
- * This file must be used under the terms of the CeCILL-C:
- * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
- * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
- *
- */
-
-/*-------------------------------------------------------------------------*/
 /* amiTwig.engine                                                          */
 /*-------------------------------------------------------------------------*/
 
@@ -2320,6 +2209,135 @@ amiTwig.expr.cache = {
 		if(!_) _ = {};
 
 		return f.call(_, _);
+
+		/*---------------------------------------------------------*/
+	},
+
+	/*-----------------------------------------------------------------*/
+};
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ * AMI TWIG Engine
+ *
+ * Copyright (c) 2014-2017 The AMI Team / LPSC / IN2P3
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*-------------------------------------------------------------------------*/
+/* amiTwig.date                                                            */
+/*-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ * AMI TWIG Engine
+ *
+ * Copyright (c) 2014-2017 The AMI Team / LPSC / IN2P3
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*-------------------------------------------------------------------------*/
+/* amiTwig.ajax                                                            */
+/*-------------------------------------------------------------------------*/
+
+/**
+ * The AMI TWIG Ajax
+ * @namespace ami/twig/ajax
+ */
+
+amiTwig.ajax = {
+	/*-----------------------------------------------------------------*/
+
+	dict: {},
+
+	/*-----------------------------------------------------------------*/
+
+	get: function(url, done, fail)
+	{
+		var txt;
+
+		/*---------------------------------------------------------*/
+
+		if(url in this.dict)
+		{
+			if(done)
+			{
+				done(this.dict[url]);
+			}
+		}
+
+		/*---------------------------------------------------------*/
+
+		if(amiTwig.fs)
+		{
+			/*-------------------------------------------------*/
+			/* NODEJS                                          */
+			/*-------------------------------------------------*/
+
+			try
+			{
+				txt = this.dict[url] = amiTwig.fs.readFileSync(url, 'utf8');
+
+				if(done)
+				{
+					done(txt);
+				}
+			}
+			catch(err)
+			{
+				if(fail)
+				{
+					fail(err);
+				}
+			}
+
+			/*-------------------------------------------------*/
+		}
+		else
+		{
+			/*-------------------------------------------------*/
+			/* BROWSER                                         */
+			/*-------------------------------------------------*/
+
+			var xmlHttpRequest = new XMLHttpRequest();
+
+			xmlHttpRequest.open('GET', url, false);
+			xmlHttpRequest.send();
+
+			/*-------------------------------------------------*/
+
+			if(xmlHttpRequest.status === 200)
+			{
+				txt = this.dict[url] = xmlHttpRequest.responseText;
+
+				if(done)
+				{
+					done(txt);
+				}
+			}
+			else
+			{
+				txt = /**************/ xmlHttpRequest.responseText;
+
+				if(fail)
+				{
+					fail(txt);
+				}
+			}
+
+			/*-------------------------------------------------*/
+		}
 
 		/*---------------------------------------------------------*/
 	},
