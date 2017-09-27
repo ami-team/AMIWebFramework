85c87b34888b1322044c63f8259c4dfa565527a2
Jérôme Odier <odier.jerome@gmail.com>
2017-09-27 13:33:31 +0200

    update

---
+++ controls/Tab/js/TabCtrl.js
@@ -0,0 +1,41 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / IN2P3
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*-------------------------------------------------------------------------*/
+
+$AMIClass('TabCtrl', {
+	/*-----------------------------------------------------------------*/
+
+	$extends: ami.Control,
+
+	/*-----------------------------------------------------------------*/
+
+	$init: function()
+	{
+		this.$super.$init();
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	onReady: function()
+	{
+		return amiWebApp.loadResources([
+			amiWebApp.originURL + '/controls/Tab/twig/TabCtrl.twig',
+		], {context: this}).done(function(data) {
+
+			/* TODO */
+		});
+	},
+
+	/*-----------------------------------------------------------------*/
+});
+
+/*-------------------------------------------------------------------------*/
