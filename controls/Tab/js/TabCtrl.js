c150a39d985c9c59788d84a849c412add86ffeda
Jérôme Odier <odier.jerome@gmail.com>
2017-09-28 16:05:42 +0200

    update

--- controls/Tab/js/TabCtrl.js
+++ controls/Tab/js/TabCtrl.js
@@ -21,6 +21,8 @@ $AMIClass('TabCtrl', {
 	$init: function()
 	{
 		this.$super.$init();
+
+		this._cnt = 0;
 	},
 
 	/*-----------------------------------------------------------------*/
@@ -29,10 +31,126 @@ $AMIClass('TabCtrl', {
 	{
 		return amiWebApp.loadResources([
 			amiWebApp.originURL + '/controls/Tab/twig/TabCtrl.twig',
+			amiWebApp.originURL + '/controls/Tab/twig/nav_item.twig',
+			amiWebApp.originURL + '/controls/Tab/twig/tab_pane.twig',
 		], {context: this}).done(function(data) {
 
-			/* TODO */
+			this.fragmentTabCtrl = data[0];
+			this.fragmentNavItem = data[1];
+			this.fragmentTabPane = data[2];
 		});
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	render: function(selector, settings)
+	{
+		return this.replaceHTML(this._selector = selector, this.fragmentTabCtrl, settings);
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	_getTabEl: function(tabId)
+	{
+		return $(this._selector + ' .nav-link[href="#' + tabId + '"]:parent');
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	_getPanEl: function(tabId)
+	{
+		return $(this._selector + ' #' + tabId);
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	prependTab: function(title, settings)
+	{
+		var _this = this;
+
+		var result = $.Deferred();
+
+		/*---------------------------------------------------------*/
+
+		var tabId = this.patchId('F3EF6D3D_723B_F5FB_F299_E0AA9CA0914D') + '_' + this._cnt;
+
+		var dict = {
+			id: tabId,
+			title: title,
+			active: this._cnt === 0,
+		};
+
+		this._cnt++;
+
+		/*---------------------------------------------------------*/
+
+		this.prependHTML(this._selector + ' .nav-tabs', this.fragmentNavItem, {context: this, dict: dict}).done(function() {
+			this.prependHTML(this._selector + ' .tab-content', this.fragmentTabPane, {context: this, dict: dict}).done(function() {
+
+				this._getTabEl(tabId).find('.fa-times').click(function(e) {
+
+					e.preventDefault();
+
+					_this.removeTab(tabId);
+				});
+
+				result.resolveWith(settings && settings['context'] ? settings['context'] : result, [tabId]);
+			});
+		});
+
+		/*---------------------------------------------------------*/
+
+		return result.promise();
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	appendTab: function(title, settings)
+	{
+		var _this = this;
+
+		var result = $.Deferred();
+
+		/*---------------------------------------------------------*/
+
+		var tabId = this.patchId('F3EF6D3D_723B_F5FB_F299_E0AA9CA0914D') + '_' + this._cnt;
+
+		var dict = {
+			id: tabId,
+			title: title,
+			active: this._cnt === 0,
+		};
+
+		this._cnt++;
+
+		/*---------------------------------------------------------*/
+
+		this.appendHTML(this._selector + ' .nav-tabs', this.fragmentNavItem, {context: this, dict: dict}).done(function() {
+			this.appendHTML(this._selector + ' .tab-content', this.fragmentTabPane, {context: this, dict: dict}).done(function() {
+
+				this._getTabEl(tabId).find('.fa-times').click(function(e) {
+
+					e.preventDefault();
+
+					_this.removeTab(tabId);
+				});
+
+				result.resolveWith(settings && settings['context'] ? settings['context'] : result, [tabId]);
+			});
+		});
+
+		/*---------------------------------------------------------*/
+
+		return result.promise();
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	removeTab: function(tabId)
+	{
+		this._getTabEl(tabId).remove();
+
+		this._getPanEl(tabId).remove();
 	},
 
 	/*-----------------------------------------------------------------*/
