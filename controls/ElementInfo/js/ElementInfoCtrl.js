cb8a731fb8668e8a62fd2d166e9aa20a44540560
Jérôme Odier <odier.jerome@gmail.com>
2017-10-20 11:32:37 +0200

    update

---
+++ controls/ElementInfo/js/ElementInfoCtrl.js
@@ -0,0 +1,48 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / IN2P3
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*-------------------------------------------------------------------------*/
+
+$AMIClass('ElementInfoCtrl', {
+	/*-----------------------------------------------------------------*/
+
+	$extends: ami.Control,
+
+	/*-----------------------------------------------------------------*/
+
+	$init: function(parent, owner)
+	{
+		this.$super.$init(parent, owner);
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	onReady: function()
+	{
+		return amiWebApp.loadResources([
+			amiWebApp.originURL + '/controls/ElementInfo/twig/ElementInfoCtrl.twig',
+		], {context: this}).done(function(data) {
+
+			/* TODO */
+		});
+	},
+
+	/*-----------------------------------------------------------------*/
+
+	render: function(selector, catalog, entity, primaryFieldName, primaryFieldValue, settings)
+	{
+		$(selector).text('GetElementInfo -catalog="' + amiWebApp.textToString(catalog) + '" -entity="' + amiWebApp.textToString(entity) + '" -primaryFieldName="' + amiWebApp.textToString(primaryFieldName) + '" -primaryFieldValue="' + amiWebApp.textToString(primaryFieldValue) + '"');
+	},
+
+	/*-----------------------------------------------------------------*/
+});
+
+/*-------------------------------------------------------------------------*/
