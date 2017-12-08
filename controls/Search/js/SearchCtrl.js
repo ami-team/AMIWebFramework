0edce1f04ac96647a533d485df498c284d6cde2a
Jérôme Odier <odier.jerome@gmail.com>
2017-12-08 13:36:13 +0100

    update

--- controls/Search/js/SearchCtrl.js
+++ controls/Search/js/SearchCtrl.js
@@ -480,11 +480,6 @@ $AMIClass('SearchCtrl', {
 			this.ctx.mql += ' WHERE ';
 			this.ctx.mql += filter;
 		}
-		else /* BERK */
-		{
-			mql += ' WHERE 1=1';
-			this.ctx.mql += ' WHERE 1=1';
-		}
 
 		/*---------------------------------------------------------*/
 
