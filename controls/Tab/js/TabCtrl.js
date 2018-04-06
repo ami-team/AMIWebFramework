3a04737e416fda1a5d6953f96a6968822ae3475d
Jérôme Odier <odier.jerome@gmail.com>
2018-04-06 14:33:04 +0200

    update

--- controls/Tab/js/TabCtrl.js
+++ controls/Tab/js/TabCtrl.js
@@ -147,7 +147,7 @@ $AMIClass('TabCtrl', {
 		var _this = this;
 
 		var tabSelector = this._card ? '.card > .card-header > .nav-tabs' : '.nav-tabs';
-		var contentSelector = this._card ? '.card > .card-body > .nav-content' : '.nav-content';
+		var contentSelector = this._card ? '.card > .card-body > .tab-content' : '.tab-content';
 
 		this.prependHTML(this._selector + ' ' + tabSelector, this.fragmentNavItem, {context: this, dict: dict}).done(function() {
 			this.prependHTML(this._selector + ' ' + contentSelector, this.fragmentTabPane, {context: this, dict: dict}).done(function() {
@@ -234,7 +234,7 @@ $AMIClass('TabCtrl', {
 		var _this = this;
 
 		var tabSelector = this._card ? '.card > .card-header > .nav-tabs' : '.nav-tabs';
-		var contentSelector = this._card ? '.card > .card-body > .nav-content' : '.nav-content';
+		var contentSelector = this._card ? '.card > .card-body > .tab-content' : '.tab-content';
 
 		this.appendHTML(this._selector + ' ' + tabSelector, this.fragmentNavItem, {context: this, dict: dict}).done(function() {
 			this.appendHTML(this._selector + ' ' + contentSelector, this.fragmentTabPane, {context: this, dict: dict}).done(function() {
