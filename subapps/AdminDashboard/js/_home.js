f4ed2b0c84ffb96846182c1e9636659741d322b7
Odier Jérôme <odier.jerome@gmail.com>
2019-01-08 12:03:17 +0100

    update

--- subapps/AdminDashboard/js/_home.js
+++ subapps/AdminDashboard/js/_home.js
@@ -104,7 +104,7 @@ $AMIClass('AdminDashboardHome', {
 
 		/*-----------------------------------------------------------------*/
 
-		amiCommand.execute('FlushServerCaches').done(function(data) {
+		amiCommand.execute('FlushServerCaches -full').done(function(data) {
 
 			amiWebApp.success(amiWebApp.jspath('..info.$', data), true);
 
