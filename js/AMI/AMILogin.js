a31018092b08c30547f0b12a4bd74333738935ac
Odier Jérôme <odier.jerome@gmail.com>
2019-02-01 11:52:11 +0100

    update

--- js/AMI/AMILogin.js
+++ js/AMI/AMILogin.js
@@ -322,11 +322,11 @@ $AMINamespace('amiLogin', /** @lends amiLogin */ {
 
 			/*-------------------------------------------------------------*/
 
-			$('#BB07676B_EACA_9B42_ED51_477DB2976041').text(Object.keys(roleInfo).join('<br />'));
+			$('#C587486B_62C0_4B6E_9288_D8F9F89D157B').prop('disabled', vomsEnabled !== 'false');
 
 			/*-------------------------------------------------------------*/
 
-			$('#C587486B_62C0_4B6E_9288_D8F9F89D157B').prop('disabled', vomsEnabled !== 'false');
+			$('#BB07676B_EACA_9B42_ED51_477DB2976041').html(Object.keys(roleInfo).join('<br />'));
 
 			/*-------------------------------------------------------------*/
 			/* CHECK USER STATUS                                           */
