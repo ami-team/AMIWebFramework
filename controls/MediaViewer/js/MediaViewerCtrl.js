9040e4ae2202749d69ebf43cb6e0f59e2742ca43
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2019-04-12 00:42:28 +0200

    update

---
+++ controls/MediaViewer/js/MediaViewerCtrl.js
@@ -0,0 +1,42 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / CNRS
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*-------------------------------------------------------------------------*/
+
+$AMIClass('MediaViewerCtrl', {
+	/*---------------------------------------------------------------------*/
+
+	$extends: ami.Control,
+
+	/*---------------------------------------------------------------------*/
+
+	$init: function(parent, owner)
+	{
+		this.$super.$init(parent, owner);
+	},
+
+	/*---------------------------------------------------------------------*/
+
+	onReady: function()
+	{
+		return amiWebApp.loadResources([
+			amiWebApp.originURL + '/controls/MediaViewer/css/MediaViewerCtrl.css',
+			amiWebApp.originURL + '/controls/MediaViewer/twig/MediaViewerCtrl.twig',
+		], {context: this}).done(function(data) {
+
+			/* TODO */
+		});
+	},
+
+	/*---------------------------------------------------------------------*/
+});
+
+/*-------------------------------------------------------------------------*/
