217772bbef69841c5db94b78bf7bf3c94892cc9a
Odier Jérôme <odier.jerome@gmail.com>
2019-10-25 16:39:51 +0200

    update

---
+++ subapps/TableViewer/js/TableViewerApp.es6.js
@@ -0,0 +1,84 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / IN2P3
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+$AMIClass('TableViewerApp', {
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	$extends: ami.SubApp,
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	onReady: function(userdata)
+	{
+		const result = $.Deferred();
+
+		amiWebApp.loadResources([
+			'subapps/TableViewer/twig/TableViewerApp.twig',
+			'ctrl:table',
+		]).done((data) => {
+
+			amiWebApp.replaceHTML('#ami_main_content', data[0]).done(() => {
+
+				try
+				{
+					json = JSON.parse(userdata || '{}');
+				}
+				catch(e)
+				{
+					json = {/*----------------------*/};
+				}
+
+				this.catalog = json.catalog || '';
+				this.entity = json.entity || '';
+				this.expression = json.expression || '1=1';
+				this.primaryField = json.primaryField || '';
+
+				this.table = new data[1]();
+
+				result.resolve();
+			});
+
+		}).fail((data) => {
+
+			result.reject(data);
+		});
+
+		return result;
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	onLogin: function()
+	{
+		$('#A2944C0A_9249_E4D2_3679_494C1A3AAAF0').show();
+
+		this.table.render('#A2944C0A_9249_E4D2_3679_494C1A3AAAF0', 'BrowseQuery -catalog="' + amiWebApp.textToString(this.catalog) + '" -entity="' + amiWebApp.textToString(this.entity) + '" -mql="SELECT `*` WHERE ' + this.expression + '"', {showDetails: true, canEdit: amiLogin.hasRole('AMI_ADMIN'), catalog: this.catalog, entity: this.entity, primaryField: this.primaryField, start: 1, stop: 20, card: true});
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	onLogout: function()
+	{
+		$('#A2944C0A_9249_E4D2_3679_494C1A3AAAF0').hide();
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+});
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* GLOBAL INSTANCE                                                                                                    */
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const tableViewerApp = new TableViewerApp();
+
+/*--------------------------------------------------------------------------------------------------------------------*/
