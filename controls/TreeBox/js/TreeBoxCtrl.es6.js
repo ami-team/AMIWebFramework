8982543de654b9862f73149e321197900a5d1d18
Lambert <fabian.lambert@lpsc.in2p3.fr>
2020-05-20 18:02:09 +0200

    new TreeBox component

---
+++ controls/TreeBox/js/TreeBoxCtrl.es6.js
@@ -0,0 +1,92 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / CNRS
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+$AMIClass('TreeBoxCtrl', {
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	$extends: ami.Control,
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	$init: function(parent, owner)
+	{
+		this.$super.$init(parent, owner);
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	onReady: function()
+	{
+		return amiWebApp.loadResources([
+			amiWebApp.originURL + '/controls/TreeBox/twig/TreeBoxCtrl.twig',
+			amiWebApp.originURL + '/js/3rd-party/jsonview.js',
+			amiWebApp.originURL + '/css/3rd-party/jsonview.css',
+		]).done((data) => {
+
+			amiWebApp.appendHTML('body', data[0]).done(() => {
+                const _class = this.$class;
+
+                /*----------------------------------------------------------------------------------------------------*/
+
+                $('#E5A34976_AC6F_5B5F_770F_F26DD1A2AB96').on('hidden.bs.modal', () => {
+
+                    _class.deferred.resolveWith(_class.context || _class.deferred);
+                });
+			});
+		});
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+		render: function(json, settings)
+    	{
+    		const deferred = $.Deferred();
+
+    		/*-----------------------------------------------------------------*/
+
+    		const [
+    			context, title
+    		] = amiWebApp.setup(
+    			['context', 'title'],
+    			[deferred, 'Tree box'],
+    			settings
+    		);
+
+    		/*-----------------------------------------------------------------*/
+
+    		amiWebApp.unlock();
+
+    		$('#E262C0A2_6C07_3B1A_9774_8AE41B7C4CA6').empty();
+
+            jsonView.format(json, '#E262C0A2_6C07_3B1A_9774_8AE41B7C4CA6');
+
+    		$('#E5A34976_AC6F_5B5F_770F_F26DD1A2AB96').modal('show');
+
+    		this.$class.deferred = deferred;
+    		this.$class.context = context;
+
+    		/*-----------------------------------------------------------------*/
+
+    		return deferred.promise();
+
+    		/*-----------------------------------------------------------------*/
+    	},
+
+        show: function(text, settings)
+        {
+            return this.render(text, settings);
+        },
+
+});
+
+/*--------------------------------------------------------------------------------------------------------------------*/
