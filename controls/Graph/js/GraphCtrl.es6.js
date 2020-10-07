ffa1fc48de995f6c06996d0085a9eb2492f12fcc
Lambert <fabian.lambert@lpsc.in2p3.fr>
2020-10-07 12:23:03 +0200

    Bug fix unit editor with negative values

--- controls/Graph/js/GraphCtrl.es6.js
+++ controls/Graph/js/GraphCtrl.es6.js
@@ -43,7 +43,7 @@ $AMIClass('GraphCtrl', {
 
 	/*----------------------------------------------------------------------------------------------------------------*/
 
-	render: function(selector, command, settings)
+	render_old: function(selector, command, settings)
 	{
 		const result = $.Deferred();
 
@@ -117,8 +117,137 @@ $AMIClass('GraphCtrl', {
 		/*------------------------------------------------------------------------------------------------------------*/
 
 		return result.promise();
-	}
+	},
 
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+    render: function(selector, command, settings)
+    {
+
+		const result = $.Deferred();
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		const [context] = amiWebApp.setup(['context'], [result], settings);
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		amiCommand.execute(command).done((data) => {
+
+			this.dotString = amiWebApp.jspath('..rowset{.@type==="graph"}.row.field{.@name==="dot"}.$', data)[0] || '';
+
+			display().done(() => {
+
+				$(this.patchId('#A8E7C88D_7B78_B221_0BCB_6EF1F9CC3C15')).change((e) => {
+
+					e.preventDefault();
+
+					this.switchOrientation();
+				});
+
+				result.resolveWith(context, [data]);
+
+			});
+
+		}).fail((data) => {
+
+			result.rejectWith(context, [data]);
+		});
+
+	/*------------------------------------------------------------------------------------------------------------*/
+
+		return result.promise();
+
+    },
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+    display: function()
+    {
+    	/*------------------------------------------------------------------------------------------------------------*/
+
+        const result = $.Deferred();
+
+        /*------------------------------------------------------------------------------------------------------------*/
+
+		const dict = {
+			graph : typeof Viz !== 'undefined' ? Viz(this.dotString, 'svg') : '',
+		};
+
+		/*------------------------------------------------------------------------------------------------------------*/
+		/* GRAPH POST TREATMENT                                                                                       */
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		dict.graph = dict.graph.replace(/xlink:href="([^"]*)"/g, (x, json) => {
+
+			const jsonbObj = JSON.parse(amiWebApp.htmlToText(json));
+
+			const attrs = [
+				'data-ctrl="' + amiWebApp.textToHtml(jsonbObj['data-ctrl']) + '"',
+				'data-ctrl-location="' + amiWebApp.textToHtml(jsonbObj['data-ctrl-location']) + '"',
+				'data-params="' + amiWebApp.textToHtml(JSON.stringify(jsonbObj['data-params'])) + '"',
+				'data-settings="' + amiWebApp.textToHtml(JSON.stringify(jsonbObj['data-settings'])) + '"',
+				'data-icon="' + amiWebApp.textToHtml(jsonbObj['data-icon']) + '"',
+				'data-title="' + amiWebApp.textToHtml(jsonbObj['data-title']) + '"',
+				'data-title-icon="' + amiWebApp.textToHtml(jsonbObj['data-title-icon']) + '"',
+			];
+
+			return 'xlink:href="#" ' + attrs.join(' ');
+		});
+
+		/*--------------------------------------------------------------------------------------------------------*/
+
+		const doc = new DOMParser().parseFromString(dict.graph, 'image/svg+xml');
+
+		const svg = $(doc.documentElement);
+
+		svg.find('a[data-title-icon]').each((i, el) => {
+
+			$('<tspan font-family="FontAwesome">' + String.fromCharCode('0x' + $(el).attr('data-title-icon')) + '</tspan><tspan> </tspan>').prependTo($(el).find('text'));
+		});
+
+		dict.graph = doc.documentElement.outerHTML;
+
+		/*--------------------------------------------------------------------------------------------------------*/
+
+		this.replaceHTML(selector, this.fragmentGraphCtrl, {dict: dict}).done(() => {
+
+			$(selector + ' a[data-ctrl]').click((e) => {
+
+				e.preventDefault();
+
+				this.createControlFromWebLink(this.getParent(), e.currentTarget, this.ctx);
+			});
+
+			result.resolveWith(context, [data]);
+		});
+
+    },
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	switchOrientation: function()
+    {
+     	const result = $.Deferred();
+
+     	/*------------------------------------------------------------------------------------------------------------*/
+
+     	let json = JSON.parse(this.dotString);
+
+     	if(json.graph['rankdir'] === 'LR' )
+     	{
+     		json.graph['rankdir'] = 'TB';
+     	}
+     	else
+     	{
+     		json.graph['rankdir'] = 'LR';
+     	}
+
+     	this.dotString = JSON.stringify(json);
+
+		display().done(() => {
+			result.resolveWith(context);
+		});
+    }
 	/*----------------------------------------------------------------------------------------------------------------*/
 });
 
