64eda92dbb4431ee04c56dbf694b78c1c8e92262
Lambert <fabian.lambert@lpsc.in2p3.fr>
2020-10-13 15:54:07 +0200

    graphviz -> png

--- controls/Graph/js/GraphCtrl.es6.js
+++ controls/Graph/js/GraphCtrl.es6.js
@@ -59,6 +59,8 @@ $AMIClass('GraphCtrl', {
 
 			this.dotString = amiWebApp.jspath('..rowset{.@type==="graph"}.row.field{.@name==="dot"}.$', data)[0] || '';
 
+			console.debug(this.dotString);
+
 			this.replaceHTML(selector, this.fragmentGraphCtrl).done(() => {
 
 				/*----------------------------------------------------------------------------------------------------*/
