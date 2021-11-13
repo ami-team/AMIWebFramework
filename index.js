2bcf4cd5aa262231e5e8a7f4f8fec7e460104d2d
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-11-13 19:36:37 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 import {$AMIClass, $AMINamespace, $AMIInterface} from './src/core/AMIObject';
-import amiInterface from './src/core/AMIInterface';
+
+/**/
+
 import amiCommand from './src/core/AMICommand';
 import amiWebApp from './src/core/AMIWebApp';
 import amiAuth from './src/core/AMIAuth';
 import amiDoc from './src/core/AMIDoc';
+
+/**/
+
+import './src/core/AMIInterface';
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -13,9 +19,11 @@ window.$AMIClass = $AMIClass;
 window.$AMINamespace = $AMINamespace;
 window.$AMIInterface = $AMIInterface;
 
-window.amiInterface = amiInterface;
+/**/
+
 window.amiCommand = amiCommand;
 window.amiWebApp = amiWebApp;
+window.amiLogin = amiAuth;
 window.amiAuth = amiAuth;
 window.amiDoc = amiDoc;
 
