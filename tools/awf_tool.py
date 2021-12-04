3bbb2b07401cb698fbca2dae2b49454ff22ddcf0
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-12-04 12:44:35 +0100

    update

--- tools/awf_tool.py
+++ tools/awf_tool.py
@@ -391,13 +391,15 @@ def updateAWF(inDebugMode, awfGITCommitId, verbose):
 
         for index, package in enumerate(PACKAGES):
 
-            if index >= 0 and 'dependencies' in package:
+            if index >= 0:
+                if 'dependencies' in package:
 
-                USER_PACKAGE_JSON['dependencies'].update(package['dependencies'])
+                    USER_PACKAGE_JSON['dependencies'].update(package['dependencies'])
 
-            if index == 0 and 'devDependencies' in package:
+            else:
+                if 'devDependencies' in package:
 
-                USER_PACKAGE_JSON['devDependencies'] = dict(package['devDependencies'])
+                    USER_PACKAGE_JSON['devDependencies'] = dict(package['devDependencies'])
 
         ################################################################################################################
 
