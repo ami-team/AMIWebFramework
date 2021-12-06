6661398828fa369f59b350d54884fd218c2436cb
Lambert <fabian.lambert@lpsc.in2p3.fr>
2021-12-06 17:13:32 +0100

    adding textbox control

---
+++ controls/TextBox/TextBoxCtrl.es6.js
@@ -0,0 +1,145 @@
+/*!
+ * AMI Web Framework
+ *
+ * Copyright (c) 2014-XXXX The AMI Team / LPSC / CNRS
+ *
+ * This file must be used under the terms of the CeCILL-C:
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+ * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
+ *
+ */
+
+import twigTextBoxCtrl from './assets/twig/TextBoxCtrl.twig';
+
+import ClipboardJS from 'clipboard';
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+$AMIClass('TextBoxCtrl', {
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	$extends: ami.Control,
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	$init: function(parent, owner)
+	{
+		this.$super.$init(parent, owner);
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	onReady: function()
+	{
+		const result = $.Deferred();
+
+		amiWebApp.appendHTML('body', twigTextBoxCtrl).done(() => {
+
+			const _class = this.$class;
+
+			/*--------------------------------------------------------------------------------------------------------*/
+
+			$('#B8927006_7FCE_87BD_FC8D_C7575D69C362').on('hidden.bs.modal', () => {
+
+				amiWebApp.modalLeave();
+
+				_class.deferred.resolveWith(_class.context || _class.deferred);
+			});
+
+			/*--------------------------------------------------------------------------------------------------------*/
+
+			new ClipboardJS('#B8927006_7FCE_87BD_FC8D_C7575D69C362 .btn[data-clipboard-target]');
+
+			/*--------------------------------------------------------------------------------------------------------*/
+
+			result.resolve();
+
+			/*--------------------------------------------------------------------------------------------------------*/
+		});
+
+		return result;
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	render: function(text, options)
+	{
+		const deferred = $.Deferred();
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		const [
+			context, title, lang
+		] = amiWebApp.setup(
+			['context', 'title', 'lang'],
+			[deferred, 'Text box', 'text'],
+			options
+		);
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		if(lang === 'json')
+		{
+			try
+			{
+				if(amiWebApp.typeOf(text) !== 'String')
+				{
+					text = JSON.stringify(/*------*/(text), null, 2);
+				}
+				else
+				{
+					text = text.trim();
+
+					if(text && text.toUpperCase() !== '@NULL')
+					{
+						text = JSON.stringify(JSON.parse(text), null, 2);
+					}
+					else
+					{
+						text = 'null';
+					}
+				}
+			}
+			catch(e)
+			{
+				amiWebApp.error('invalid JSON string', true);
+
+				text = 'null';
+			}
+		}
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		$('#B6FA759D_D2DD_D079_B591_5023C422B87F').text(title);
+
+		const editor = $('#B8927006_7FCE_87BD_FC8D_C7575D69C362 .form-editor').val(text).data('editor');
+
+		$('#B8927006_7FCE_87BD_FC8D_C7575D69C362').modal('show');
+
+		this.$class.deferred = deferred;
+		this.$class.context = context;
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		window.monaco.editor.setModelLanguage(editor.getModel(), lang);
+
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		amiWebApp.modalEnter();
+
+		return deferred.promise();
+
+		/*------------------------------------------------------------------------------------------------------------*/
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+	show: function(text, options)
+	{
+		return this.render(text, options);
+	},
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+});
+
+/*--------------------------------------------------------------------------------------------------------------------*/
