126ba19de6a84e82a9461eb5a9a2acbddfb6cc50
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-12-07 12:03:03 +0100

    update

--- subapps/AdminDashboard/commands.js
+++ subapps/AdminDashboard/commands.js
@@ -10,6 +10,11 @@
  */
 
 import twigCommands from './assets/twig/commands/commands.twig';
+import twigTable    from './assets/twig/commands/table.twig'   ;
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+let roles = {};
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -19,6 +24,19 @@ export function init()
 
 	amiWebApp.replaceHTML('#BCCE2136_3695_AB6F_4F08_3BD3C9035287', twigCommands).done(() => {
 
+		$('#FA9E8ABC_2469_C3CA_67F9_8DC2FEE6B32D').keypress((e) => {
+
+			if(e.keyCode === 13)
+			{
+				find($('#FA9E8ABC_2469_C3CA_67F9_8DC2FEE6B32D').val());
+			}
+		});
+
+		$('#CD24BC02_C39F_AE0C_A83F_85B6458421B6').click(() => {
+
+			find($('#FA9E8ABC_2469_C3CA_67F9_8DC2FEE6B32D').val());
+		});
+
 		result.resolve();
 	});
 
@@ -27,35 +45,226 @@ export function init()
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-export function onExit()
-{
-	const result = $.Deferred();
-
-	result.resolve();
-
-	return result;
-}
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-
 export function onLogin()
 {
-	const result = $.Deferred();
+	return amiCommand.execute('SearchQuery -catalog="self" -entity="router_role" -mql="SELECT `router_role`.`role`"').done((data) => {
 
-	result.resolve();
+		roles = amiWebApp.jspath('..field{.@name==="role"}.$', data);
 
-	return result;
+	}).fail((data, message) => {
+
+		amiWebApp.error(message, true);
+	});
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-export function onLogout()
+function find(filter)
 {
-	const result = $.Deferred();
+	amiWebApp.lock();
 
-	result.resolve();
+	filter = filter.trim().toLowerCase();
 
-	return result;
+	amiCommand.execute('SearchQuery -catalog="self" -entity="router_command" -mql="SELECT `router_command`.`id`, `router_role`.`role` WHERE LOWER(`router_command`.`command`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\'"').done((data2) => {
+
+		amiCommand.execute('SearchQuery -catalog="self" -entity="router_command" -mql="SELECT `id`, `command`, `class`, `visible`, `secured`, `roleValidatorClass` WHERE LOWER(`command`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' ORDER BY `command` LIMIT 50"').done((data1) => {
+
+			const rows1 = amiWebApp.jspath('..rowset.row', data1);
+			const rows2 = amiWebApp.jspath('..rowset.row', data2);
+
+			const ids = [];
+
+			const commands = {};
+
+			rows1.forEach((row) => {
+
+				const id = amiWebApp.jspath('..field{.@name==="id"}.$', row)[0] || '';
+				const command = amiWebApp.jspath('..field{.@name==="command"}.$', row)[0] || '';
+				const clazz = amiWebApp.jspath('..field{.@name==="class"}.$', row)[0] || '';
+				const visible = amiWebApp.jspath('..field{.@name==="visible"}.$', row)[0] || '';
+				const roleValidatorClazz = amiWebApp.jspath('..field{.@name==="roleValidatorClass"}.$', row)[0] || '';
+
+				ids.push(id);
+
+				commands[id] = {
+					command: command,
+					clazz: clazz,
+					visible: visible,
+					roleValidatorClazz: roleValidatorClazz,
+					roles: [],
+				};
+			});
+
+			rows2.forEach((row) => {
+
+				const id = amiWebApp.jspath('..field{.@name==="id"}.$', row)[0] || '';
+				const role = amiWebApp.jspath('..field{.@name==="self.router_role.role"}.$', row)[0] || '';
+
+				if(id in commands)
+				{
+					commands[id].roles.push(role);
+				}
+			});
+
+			const dict = {
+				ids: ids,
+				roles: roles,
+				commands: commands,
+			};
+
+			amiWebApp.replaceHTML('#F989424E_06B3_365D_0CAD_9F223EC8DA01', twigTable, {dict: dict}).done(() => {
+
+				const el1 = $('#F989424E_06B3_365D_0CAD_9F223EC8DA01 [data-change-visible]');
+
+				const el2 = $('#F989424E_06B3_365D_0CAD_9F223EC8DA01 [data-delete-command]');
+
+				const el3 = $('#F989424E_06B3_365D_0CAD_9F223EC8DA01 [data-change-role]');
+
+				const el4 = $('#F989424E_06B3_365D_0CAD_9F223EC8DA01 [data-change-command-class]');
+
+				const el5 = $('#F989424E_06B3_365D_0CAD_9F223EC8DA01 [data-change-role-validator-class]');
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.select2({
+					placeholder: 'Select a role',
+				});
+
+				/*-----------------------------------------------------------------------------------------------------*/
+
+				el1.change((e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('UpdateElements -catalog="self" -entity="router_command" -separator="|" -fields="visible" -values=? -keyFields="id" -keyValues=?', {params: [$(e.currentTarget).prop('checked') ? 1 : 0, $(e.currentTarget).attr('data-change-visible')]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail((data, message) => {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el2.click((e) => {
+
+					e.preventDefault();
+
+					if(!confirm('Are you sure?'))
+					{
+						return;
+					}
+
+					amiWebApp.lock();
+
+					amiCommand.execute('RemoveElements -catalog="self" -entity="router_command" -keyFields="id" -keyValues=?', {params: [$(e.currentTarget).attr('data-delete-command')]}).done(() => {
+
+						$(`#F989424E_06B3_365D_0CAD_9F223EC8DA01 tr[data-id="${$(e.currentTarget).attr('data-delete-command')}"]`).remove();
+
+						amiWebApp.unlock();
+
+					}).fail((data, message) => {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.on('select2:select', (e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('AddCommandRole -command=? -role=?', {params: [$(e.currentTarget).attr('data-change-role'), e.params.data.text]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail((data, message) => {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.on('select2:unselect', (e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('RemoveCommandRole -command=? -role=?', {params: [$(e.currentTarget).attr('data-change-role'), e.params.data.text]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail((data, message) => {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el4.on('click', (e) => {
+
+					const clazz = prompt('New Java command class', $(e.currentTarget).find('.value').text());
+
+					if(clazz !== null)
+					{
+						amiWebApp.lock();
+
+						amiCommand.execute('UpdateElements -catalog="self" -entity="router_command" -separator="|" -fields="class" -values=? -keyFields="id" -keyValues=?', {params: [clazz, $(e.currentTarget).attr('data-change-command-class')]}).done(() => {
+
+							el4.find('.value').text(clazz);
+
+							amiWebApp.unlock();
+
+						}).fail((data, message) => {
+
+							amiWebApp.error(message, true);
+						});
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el5.on('click', (e) => {
+
+					const clazz = prompt('New Java role validator class', $(e.currentTarget).find('.value').text());
+
+					if(clazz !== null)
+					{
+						amiWebApp.lock();
+
+						amiCommand.execute('UpdateElements -catalog="self" -entity="router_command" -separator="|" -fields="roleValidatorClass" -values=? -keyFields="id" -keyValues=?', {params: [clazz, $(e.currentTarget).attr('data-change-role-validator-class')]}).done(() => {
+
+							el5.find('.value').text(clazz);
+
+							amiWebApp.unlock();
+
+						}).fail((data, message) => {
+
+							amiWebApp.error(message, true);
+						});
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				amiWebApp.unlock();
+
+				/*----------------------------------------------------------------------------------------------------*/
+			});
+
+		}).fail((data, message) => {
+
+			amiWebApp.error(message, true);
+		});
+
+	}).fail((data, message) => {
+
+		amiWebApp.error(message, true);
+	});
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
--- subapps/AdminDashboard/users.js
+++ subapps/AdminDashboard/users.js
@@ -10,6 +10,11 @@
  */
 
 import twigUsers from './assets/twig/users/users.twig';
+import twigTable from './assets/twig/users/table.twig';
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+let roles = {};
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -19,6 +24,19 @@ export function init()
 
 	amiWebApp.replaceHTML('#BCCE2136_3695_AB6F_4F08_3BD3C9035287', twigUsers).done(() => {
 
+		$('#AB0061DA_691D_80A5_C2C0_2286ABB4845D').keypress((e) => {
+
+			if(e.keyCode === 13)
+			{
+				find($('#AB0061DA_691D_80A5_C2C0_2286ABB4845D').val());
+			}
+		});
+
+		$('#CD24BC02_C39F_AE0C_A83F_85B6458421B6').click(() => {
+
+			find($('#AB0061DA_691D_80A5_C2C0_2286ABB4845D').val());
+		});
+
 		result.resolve();
 	});
 
@@ -27,35 +45,261 @@ export function init()
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-export function onExit()
-{
-	const result = $.Deferred();
-
-	result.resolve();
-
-	return result;
-}
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-
 export function onLogin()
 {
-	const result = $.Deferred();
+	return amiCommand.execute('SearchQuery -catalog="self" -entity="router_role" -mql="SELECT `router_role`.`role`"').done((data) => {
 
-	result.resolve();
+		roles = amiWebApp.jspath('..field{.@name==="role"}.$', data);
 
-	return result;
+	}).fail((data, message) => {
+
+		amiWebApp.error(message, true);
+	});
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-export function onLogout()
+function find(filter)
 {
-	const result = $.Deferred();
+	amiWebApp.lock();
 
-	result.resolve();
+	filter = (filter || '').trim().toLowerCase();
 
-	return result;
+	amiCommand.execute('SearchQuery -catalog="self" -entity="router_user" -mql="SELECT `router_user`.`id`, `router_role`.`role` WHERE LOWER(`router_user`.`AMIUser`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' OR LOWER(`router_user`.`ssoUser`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' OR LOWER(`router_user`.`email`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\'"' ).done((data2) => {
+
+		amiCommand.execute('SearchQuery -catalog="self" -entity="router_user" -mql="SELECT `id`, `AMIUser`, `ssoUser`, `clientDN`, `issuerDN`, `firstName`, `lastName`, `email`, `json`, `valid` WHERE LOWER(`AMIUser`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' OR LOWER(`router_user`.`ssoUser`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' OR LOWER(`email`) LIKE \'%' + amiWebApp.textToString(amiWebApp.textToSQL(filter)) + '%\' ORDER BY `AMIUser` LIMIT 50"').done((data1) => {
+
+			const rows1 = amiWebApp.jspath('..rowset.row', data1);
+			const rows2 = amiWebApp.jspath('..rowset.row', data2);
+
+			const ids = [];
+
+			const users = {};
+
+			rows1.forEach(function(row) {
+
+				const id = amiWebApp.jspath('..field{.@name==="id"}.$', row)[0] || '';
+				const AMIUser = amiWebApp.jspath('..field{.@name==="AMIUser"}.$', row)[0] || '';
+				const ssoUser = amiWebApp.jspath('..field{.@name==="ssoUser"}.$', row)[0] || '';
+				const clientDN = amiWebApp.jspath('..field{.@name==="clientDN"}.$', row)[0] || '';
+				const issuerDN = amiWebApp.jspath('..field{.@name==="issuerDN"}.$', row)[0] || '';
+				const firstname = amiWebApp.jspath('..field{.@name==="firstName"}.$', row)[0] || '';
+				const lastname = amiWebApp.jspath('..field{.@name==="lastName"}.$', row)[0] || '';
+				const email = amiWebApp.jspath('..field{.@name==="email"}.$', row)[0] || '';
+				const json = amiWebApp.jspath('..field{.@name==="json"}.$', row)[0] || '';
+				const valid = amiWebApp.jspath('..field{.@name==="valid"}.$', row)[0] || '';
+
+				ids.push(id);
+
+				users[id] = {
+					AMIUser: AMIUser,
+					ssoUser: ssoUser,
+					clientDN: clientDN,
+					issuerDN: issuerDN,
+					firstname: firstname,
+					lastname: lastname,
+					email: email,
+					json: json,
+					valid: valid,
+					roles: [],
+				};
+			});
+
+			rows2.forEach(function(row) {
+
+				const id = amiWebApp.jspath('..field{.@name==="id"}.$', row)[0] || '';
+				const role = amiWebApp.jspath('..field{.@name==="self.router_role.role"}.$', row)[0] || '';
+
+				if(id in users)
+				{
+					users[id].roles.push(role);
+				}
+			});
+
+			const dict = {
+				ids: ids,
+				users: users,
+				roles: roles,
+			};
+
+			amiWebApp.replaceHTML('#FCBD6DFC_2061_8CC3_652C_77671A179AAC', twigTable, {dict: dict}).done(() => {
+
+				const el1 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-change-valid]');
+
+				const el2 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-delete-user]');
+
+				const el3 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-change-role]');
+
+				const el4 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-change-json]');
+
+				const el5 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-change-client-dn]');
+
+				const el6 = $('#FCBD6DFC_2061_8CC3_652C_77671A179AAC [data-change-issuer-dn]');
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.select2({
+					placeholder: 'Select a role',
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el1.change((e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('UpdateElements -catalog="self" -entity="router_user" -separator="|" -fields="valid" -values=? -keyFields="id" -keyValues=?', {params: [$(e.currentTarget).prop('checked') ? 1 : 0, $(e.currentTarget).attr('data-change-valid')]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail(function(data, message) {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el2.click((e) => {
+
+					e.preventDefault();
+
+					if(!confirm('Are you sure?'))
+					{
+						return;
+					}
+
+					amiWebApp.lock();
+
+					amiCommand.execute('RemoveElements -catalog="self" -entity="router_user" -keyFields="id" -keyValues=?', {params: [$(e.currentTarget).attr('data-delete-user')]}).done(() => {
+
+						$(`#FCBD6DFC_2061_8CC3_652C_77671A179AAC tr[data-id="${$(e.currentTarget).attr('data-delete-user')}"]`).remove();
+
+						amiWebApp.unlock();
+
+					}).fail(function(data, message) {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.on('select2:select', (e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('AddUserRole -user=? -role=?', {params: [$(e.currentTarget).attr('data-change-role'), e.params.data.text]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail(function(data, message) {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el3.on('select2:unselect', (e) => {
+
+					amiWebApp.lock();
+
+					amiCommand.execute('RemoveUserRole -user=? -role=?', {params: [$(e.currentTarget).attr('data-change-role'), e.params.data.text]}).done(() => {
+
+						amiWebApp.unlock();
+
+					}).fail(function(data, message) {
+
+						amiWebApp.error(message, true);
+					});
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el4.on('click', (e) => {
+
+					const clientDN = prompt('New JSON', $(e.currentTarget).find('.value').text());
+
+					if(clientDN)
+					{
+						amiWebApp.lock();
+
+						amiCommand.execute('UpdateElements -catalog="self" -entity="router_user" -separator="|" -fields="json" -values=? -keyFields="id" -keyValues=?', {params: [clientDN, $(e.currentTarget).attr('data-change-json')]}).done(() => {
+
+							el4.find('.value').text(clientDN);
+
+							amiWebApp.unlock();
+
+						}).fail(function(data, message) {
+
+							amiWebApp.error(message, true);
+						});
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el5.on('click', (e) => {
+
+					const clientDN = prompt('New client DN', $(e.currentTarget).find('.value').text());
+
+					if(clientDN)
+					{
+						amiWebApp.lock();
+
+						amiCommand.execute('UpdateElements -catalog="self" -entity="router_user" -separator="|" -fields="clientDN" -values=? -keyFields="id" -keyValues=?', {params: [clientDN, $(e.currentTarget).attr('data-change-client-dn')]}).done(() => {
+
+							el5.find('.value').text(clientDN);
+
+							amiWebApp.unlock();
+
+						}).fail(function(data, message) {
+
+							amiWebApp.error(message, true);
+						});
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				el6.on('click', (e) => {
+
+					const issuerDN = prompt('New issuer DN', $(e.currentTarget).find('.value').text());
+
+					if(issuerDN)
+					{
+						amiWebApp.lock();
+
+						amiCommand.execute('UpdateElements -catalog="self" -entity="router_user" -separator="|" -fields="issuerDN" -values=? -keyFields="id" -keyValues=?', {params: [issuerDN, $(e.currentTarget).attr('data-change-issuer-dn')]}).done(() => {
+
+							el6.find('.value').text(issuerDN);
+
+							amiWebApp.unlock();
+
+						}).fail((data, message) => {
+
+							amiWebApp.error(message, true);
+						});
+					}
+				});
+
+				/*----------------------------------------------------------------------------------------------------*/
+
+				amiWebApp.unlock();
+
+				/*----------------------------------------------------------------------------------------------------*/
+			});
+
+		}).fail((data, message) => {
+
+			amiWebApp.error(message, true);
+		});
+
+	}).fail((data, message) => {
+
+		amiWebApp.error(message, true);
+	});
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
