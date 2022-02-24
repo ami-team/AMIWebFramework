402c7b933002450e8463c5ecf112473a98ad0d6f
Lambert <fabian.lambert@lpsc.in2p3.fr>
2022-02-24 22:20:00 +0100

    adder tuning

--- controls/DiffBox/DiffBoxCtrl.es6.js
+++ controls/DiffBox/DiffBoxCtrl.es6.js
@@ -7,6 +7,7 @@
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
  * http://www.cecill.info/licences/Licence_CeCILL-C_V1-fr.html
  *
+ * @global DiffMatchPatch
  */
 
 import './assets/css/DiffBoxCtrl.css';
