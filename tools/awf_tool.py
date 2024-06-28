191751b50659b1e4f8e5887e8f7a6f1dec2dc6e1
ODIER Jerome <jerome.odier@lpsc.in2p3.fr>
2024-06-28 12:40:22 +0200

    update

--- tools/awf_tool.py
+++ tools/awf_tool.py
@@ -1103,9 +1103,9 @@ const config = {
 	},
 	'output': {
 		'filename': '[name].min.js',
-		'chunkFilename': 'assets/js/chunks/[id].min.js',
 		'path': path.resolve(__dirname),
-		'assetModuleFilename': (o) => path.join(path.dirname(o.runtime), 'assets', getResourceDirectory(o.filename), '[hash][ext][query]')
+		'chunkFilename': (pathData) => path.join(path.dirname(pathData.runtime || pathData.chunk.runtime), 'assets', 'js', 'chunks', '[id].min.js'),
+		'assetModuleFilename': (pathData) => path.join(path.dirname(pathData.runtime), 'assets', getResourceDirectory(pathData.filename), '[hash][ext][query]'),
 	},
 	'devServer': {
 		'static': {
